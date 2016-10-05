9862ea09a1337a19bb861553a1e57488a4f3854c
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-10-05 17:19:01 -0300

    Minor fix on url path

--- enqueuer_api/urls.py
+++ enqueuer_api/urls.py
@@ -19,5 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
-    url(r'^accounts', include('userena.urls')),
+    url(r'^accounts/', include('userena.urls')),
 ]
