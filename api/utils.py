d992b933c038773319a290af4088a223fae5ff77
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-10-21 11:54:25 -0200

    WIP - implemented Queue's permisions

--- api/utils.py
+++ api/utils.py
@@ -1,3 +1,5 @@
+from friendship.models import Friend
+
 from userena.utils import get_user_profile
 
 def get_users_profiles(users):
@@ -7,3 +9,6 @@ def get_users_profiles(users):
     """
     users_profiles = [get_user_profile(user) for user in users]
     return users_profiles
+
+def are_friends(user_from, user_to):
+    return Friend.objects.are_friends(user_from, user_to)
