2c5eefab4ac96f33574193fef6408c1e474488f1
Antonio Carlos Falcão Petri <falcaopetri@gmail.com>
2016-10-30 21:47:49 -0200

    Added basic tests

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
 
-# Create your tests here.
+#from django.contrib.auth
+from api.models import *
+
+class UserTests(TestCase):
+    
+    def test_username_field(self):
+        user = User(username="username")
+        profile = UserProfile(user=user)
+        self.assertEquals(profile.username, user.username)
+
+
+    def test_queue_owner(self):
+        user = User()
+        user.save()
+    
+        profile = UserProfile(user=user)
+        profile.save()
+
+        queue = Queue(owner=profile)
+        queue.save()
+
+        self.assertEquals(queue.owner, profile)
+        self.assertIs(queue in profile.queues.all(), True)
